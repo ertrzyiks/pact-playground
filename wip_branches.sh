06273a4066fc5b70498a094b29be6b47df4b8cfc
ertrzyiks <mateusz.derks@gmail.com>
2020-11-12 21:03:28 +0100

    Fix wip branches script

--- wip_branches.sh
+++ wip_branches.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-curl -d ' {"consumerVersionSelectors": [{"consumer": "Provider", "tag": "production", "latest": true}],"includeWipPactsSince": "2020-10-09","includePendingStatus": true,"providerVersionTags": ["main"]}' -H 'Content-Type: application/json' http://localhost:9292/pacts/provider/Provider/for-verification
+curl -d ' {"consumerVersionSelectors": [{"tag": "production", "latest": true}],"includeWipPactsSince": "2020-10-09","includePendingStatus": true,"providerVersionTags": ["main"]}' -H 'Content-Type: application/json' http://localhost:9292/pacts/provider/Provider/for-verification
